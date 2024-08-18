3436baa579dd72ef694051314036675dbaa44d94
Mondeep Chetry <mandeepchetry6969@gmail.com>
2024-08-18 17:17:33 +0530

    dependencies

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,11 @@
 const express = require("express");
-const mainRouter = require("./routes/index");
-const userRouter = require("./routes/user");
+const cors = require("cors");
+const rootRouter = require("./routes/index");
 
+app.use(cors());
 const app = express();
 
-app.use("/api/v1", mainRouter);
-app.use("/api/v1/user", userRouter);
+app.use("/api/v1", rootRouter);
 
 
-
-
-// app.use(express.json);
-// app.post('/signup', (req, res) => {
-//     const username = req.body.username;
-//     const password = req.body.password;
-// })
+app.listen(3000)
